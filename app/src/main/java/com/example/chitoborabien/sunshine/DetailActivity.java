110b4044eb1fe0366bc4549b61de5b5184cfbdd7
renchi <chitoborabien@gmail.com>
2015-01-10 10:23:42 +0800

    Added Intent and displayed content in Details activity

---
+++ app/src/main/java/com/example/chitoborabien/sunshine/DetailActivity.java
@@ -0,0 +1,90 @@
+package com.example.chitoborabien.sunshine;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class DetailActivity extends ActionBarActivity {
+    private final String LOG_TAG = DetailActivity.class.getSimpleName();
+    String message;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+
+        // Get the message from the intent
+        Intent intent = getIntent();
+        message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE_SELECTEDFORECAST);
+
+        Log.v(LOG_TAG, message);
+
+        // Create the text view
+ /*       TextView textView = new TextView(this);
+        textView.setTextSize(40);
+        textView.setText(message);*/
+
+
+
+        setContentView(R.layout.activity_detail);
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public class PlaceholderFragment extends Fragment {
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
+
+            TextView textDate = (TextView) rootView.findViewById(R.id.textDate);
+            textDate.setText(message);
+            Log.v(LOG_TAG, message);
+
+
+            return rootView;
+        }
+    }
+}
